890a74ade4a7d1ef43a402160dc463ea90bae13e
Faisal islam shakib <faisalshakib015@gmail.com>
2025-11-13 16:36:53 +0600

    upto get post

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require('dotenv').config()
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const uri = "mongodb+srv://FoodBridge:i74SCY7uCCZ9F81G@cluster0.ro7vec0.mongodb.net/?appName=Cluster0";
+// const uri = "mongodb+srv://FoodBridge:i74SCY7uCCZ9F81G@cluster0.ro7vec0.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ro7vec0.mongodb.net/?appName=Cluster0`;
 const port = process.env.port || 3000;
 app.get("/", (req, res) => {
   res.send("Welcomes");
@@ -23,17 +25,41 @@ async function run() {
     const foodDB = client.db("FoodBridge");
      const collection = foodDB.collection("Foods");
     app.get("/foods", async (req, res) => {
-      const cursor = collection.find({});
+      const cursor = collection.find({ food_status: "Available" });
       const result = await cursor.toArray();
       res.send(result)
 
     });
+     app.get("/highestfoods", async (req, res) => {
+      const cursor = collection.find().sort({food_quantity:-1}).limit(6);
+      const result = await cursor.toArray();
+      res.send(result)
+
+    });
+     app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await collection.findOne(query);
+            res.send(result);
+        })
     app.post("/foods", async (req, res) => {
       const newFood = req.body;
 
       const result = await collection.insertOne(newFood);
       res.send(result);
     });
+    // manage Food
+     app.get("/userfood",async (req,res)=>{
+           const email = req.query.email;
+            const query = {};
+            if (email) {
+                query.donator_email = email;
+            }
+            const cursor =collection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+
+     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
